93d695ca041372d68123377f08ad9a7e03fd7d61
Adam Coddington <me@adamcoddington.net>
2017-04-28 20:18:43 -0700

    X

--- slack_frotz.py
+++ slack_frotz.py
@@ -10,7 +10,7 @@ import frotz
 app = Flask(__name__)
 if not app.debug:
     file_handler = logging.FileHandler(config.LOG_OUTPUT)
-    file_handler.setLevel(logging.WARNING)
+    file_handler.setLevel(logging.DEBUG)
     app.logger.addHandler(file_handler)
 
 
