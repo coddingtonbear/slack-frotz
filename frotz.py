6cb1c0ce47ea30f385340e2119021aff852c72a4
Adam Coddington <me@adamcoddington.net>
2017-04-28 20:35:04 -0700

    X

--- frotz.py
+++ frotz.py
@@ -97,7 +97,7 @@ class Session(object):
                 self._build_command(command),
                 timeout=5
             )
-            raise Exception(output)
+            raise Exception(err)
             if proc.returncode != 0:
                 raise FrotzError(err)
         except subprocess.TimeoutExpired:
